91948df485f495fdf583876f45456b862db60d55
c0demark <kumarvijay336@gmail.com>
2017-05-10 01:51:55 +0530

    added bot related configs

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var botbuilder = require("botbuilder");
+var chatHandler = require("./chat-handler");
+var chatbotConfig = require("./chatbot.config");
 
 var app = express();
 
@@ -8,10 +11,18 @@ app.set("port", (process.env.PORT || 8080));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get("/", function(request, response) {
-    response.status(200).send("Hi I am a chatbot");
+app.get("/", function(req, res) {
+    res.status(200).send("Hi I am a chatbot");
 });
 
 app.listen(app.get("port"), function() {
-    console.log("running port");
+    console.log("chatapp is listening on %s", app.get("host"));
 });
+
+var chatConnector = new botbuilder.ChatConnector(chatbotConfig);
+
+var chatbot = new botbuilder.UniversalBot(chatConnector);
+
+app.post("/bot/messages", chatConnector.listen());
+
+chatHandler(chatbot);
