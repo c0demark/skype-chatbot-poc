f16c8fdda026a34cbb984d8f41f6fb35e837d5d1
c0demark <kumarvijay336@gmail.com>
2017-05-10 00:07:10 +0530

    landing page url

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+
+app.set("port", (process.env.PORT || 8080));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", function(request, response) {
+    response.status(200).send("Hi I am a chatbot");
+});
