e2de53b1c456f87235e1824b28e9c0662fdad8e5
c0demark <kumarvijay336@gmail.com>
2017-05-18 19:20:47 +0530

    created bot and added configs

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+var url = require("url");
+var os = require('os');
 var express = require("express");
 var bodyParser = require("body-parser");
 var botbuilder = require("botbuilder");
@@ -6,23 +8,45 @@ var chatbotConfig = require("./chatbot.config");
 
 var app = express();
 
-app.set("port", (process.env.PORT || 8080));
+app.set("port", (process.env.port || process.env.PORT || 8888));
+// app.set("host", "localhost");
+var httpServer = app.listen(app.get("port"), function() {
+    let httpAddress = httpServer.address();
+    // console.log(httpAddress);
+    // console.log(httpServer);
+    console.log("chatapp is listening on %s:%s", httpAddress.address, httpAddress.port);
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use("/", function(req, res, next) {
+    app.set("origin", url.format({
+        protocol: req.protocol,
+        host: req.get("host"),
+    }));
+    console.log(app.get("origin"));
+    next();
+});
 
 app.get("/", function(req, res) {
     res.status(200).send("Hi I am a chatbot");
 });
 
-app.listen(app.get("port"), function() {
-    console.log("chatapp is listening on %s", app.get("host"));
-});
+// var chatConnector = null;
+// if (process.env.NODE_ENV == "dev") {
+//     chatConnector = new botbuilder.ConsoleConnector().listen();
+// } else {
+//     chatConnector = new botbuilder.ChatConnector(chatbotConfig);
+//     app.post("/api/messages", chatConnector.listen());
+// }
 
 var chatConnector = new botbuilder.ChatConnector(chatbotConfig);
 
+app.post("/api/messages", chatConnector.listen());
 var chatbot = new botbuilder.UniversalBot(chatConnector);
 
-app.post("/bot/messages", chatConnector.listen());
+// chatHandler(chatbot);
 
-chatHandler(chatbot);
+chatbot.dialog("/", [function(session) {
+    session.send(send.userData);
+}]);
